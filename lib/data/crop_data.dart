e6ec5e2f06bf414b83e030c68fb60e8c05feae6f
Lans12345 <olanalans12345@gmail.com>
2023-05-23 14:07:01 +0800

    added crop data

---
+++ lib/data/crop_data.dart
@@ -0,0 +1,76 @@
+List<Map<String, dynamic>> cropsList = [
+  {
+    'area': 'Bago',
+    'crops': [
+      {'crop': 'RICE', 'month': 'March to October'},
+      {'crop': 'SUGARCANE', 'month': 'October to May'},
+      {'crop': 'Coconut', 'month': 'May to June'},
+      {'crop': 'MANGO', 'month': 'May to August'},
+      {'crop': 'BANANA', 'month': 'February to March'}
+    ]
+  },
+  {
+    'area': 'Binalbagan',
+    'crops': [
+      {'crop': 'Sugar Cane', 'month': 'October to May'},
+      {'crop': 'Corn', 'month': 'April to May'}
+    ]
+  },
+  {
+    'area': 'Cadiz',
+    'crops': [
+      {'crop': 'Sugar Cane', 'month': 'October to May'},
+      {'crop': 'Rice', 'month': 'March to October'},
+      {'crop': 'Corn', 'month': 'April to May'}
+    ]
+  },
+  {
+    'area': 'Calatrava',
+    'crops': [
+      {'crop': 'Sugarcane Plantation', 'month': 'October to May'},
+      {'crop': 'Rice Farming', 'month': 'March to October'},
+      {'crop': 'Corn Production', 'month': 'April to May'}
+    ]
+  },
+  {
+    'area': 'Candoni',
+    'crops': [
+      {'crop': 'Pineapple', 'month': 'September to December'},
+      {'crop': 'Rice', 'month': 'March to October'}
+    ]
+  },
+  {
+    'area': 'Cauyan',
+    'crops': [
+      {'crop': 'Rice', 'month': 'March to October'},
+      {'crop': 'Corn', 'month': 'April to May'},
+      {'crop': 'Coconut', 'month': 'May to June'}
+    ]
+  },
+  {
+    'area': 'E.B Magalona',
+    'crops': [
+      {'crop': 'Sugarcane', 'month': 'No specific month'}
+    ]
+  },
+  {
+    'area': 'Escalante',
+    'crops': [
+      {'crop': 'Sugar Cane', 'month': 'October to May'}
+    ]
+  },
+  {
+    'area': 'Himamaylan',
+    'crops': [
+      {'crop': 'Sugarcane', 'month': 'October to May'},
+      {'crop': 'Rice', 'month': 'March to October'}
+    ]
+  },
+  {
+    'area': 'Hinigaran',
+    'crops': [
+      {'crop': 'Rice', 'month': 'March to October'},
+      {'crop': 'Sugar Cane', 'month': 'October to May'}
+    ]
+  }
+];
